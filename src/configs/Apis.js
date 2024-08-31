2bbabbab4c3f14bfd7a6c0b9d14f838f814920a1
advaita02 <lbnam1609@gmail.com>
2024-08-31 20:19:25 +0700

    login

--- src/configs/Apis.js
+++ src/configs/Apis.js
@@ -1,12 +1,30 @@
 import axios from "axios";
+import cookie from "react-cookies"
+
+// Đặt BASE_URL cho API
+// const BASE_URL = "http://localhost:8080/online_course/api";
 
 const SERVER_CONTEXT = "/online_course";
+const SERVER = "http://localhost:8080";
+
 export const endpoints = {
-    "categories":`${SERVER_CONTEXT}/api/categories`,
+    "categories": `${SERVER_CONTEXT}/api/categories`,
     "courses": `${SERVER_CONTEXT}/api/courses/`,
     "login": `${SERVER_CONTEXT}/api/login/`,
     "detail": (courseId) => `${SERVER_CONTEXT}/api/courses/${courseId}`,
+    "current-user": `${SERVER_CONTEXT}/api/current-user/`,
+};
+
+export const authApi = () => {
+    return axios.create({
+        baseURL: SERVER,
+        headers: {
+            "Authorization": cookie.load("token")
+        }
+    })
 }
-export default axios.create(
-    { baseURL: "http://localhost:8080" }
-)
+
+
+export default axios.create({
+    baseURL: SERVER
+});
